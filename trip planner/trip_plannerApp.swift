437482d4a9e4536e0a0ee75520c94f192d9bf24b
Handika <handika@atmatech.net>
2026-07-22 00:31:14 +0700

    Initial Commit

---
+++ trip planner/trip_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trip_plannerApp.swift
+//  trip planner
+//
+//  Created by handika on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct trip_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trip plannerTests/trip_plannerTests.swift
@@ -0,0 +1,19 @@
+//
+//  trip_plannerTests.swift
+//  trip plannerTests
+//
+//  Created by handika on 22/07/26.
+//
+
+import Testing
+@testable import trip_planner
+
+struct trip_plannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
